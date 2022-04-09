17c9777d123e16e407d78c24f089f39a760e3f78
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-04-09 01:00:45 -0300

    Refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
@@ -10,10 +11,22 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
+        <meta name="description" content="Private Chat Online" />
+        <meta name="keywords" content="chat online private" />
+        <meta name="theme-color" content="#99cc33" />
+        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
+        {/* <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
+          /> */}
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
-
-export default MyApp;
