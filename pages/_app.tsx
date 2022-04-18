80df07e0398672ac8bc08029e13b745db6f0b168
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-04-18 17:46:56 -0300

    Refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,23 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
-        <meta name="description" content="Private Chat Online" />
-        <meta name="keywords" content="chat online private" />
-        <meta name="theme-color" content="#000" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        {/* Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="crossorigin"
-        />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,23 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+        <meta name="description" content="Private Chat Online" />
+        <meta name="keywords" content="chat online private" />
+        <meta name="theme-color" content="#000" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="crossorigin"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
