f974620a2deb76f084f5a92b36c044264edc33b4
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-04-18 17:40:03 -0300

    Enable sw

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = withPWA({
   pwa: {
     dest: "public",
     // disable: process.env.NODE_ENV === "development",
-    disable: true,
+    // disable: true,
   },
   ...nextConfig,
 });
